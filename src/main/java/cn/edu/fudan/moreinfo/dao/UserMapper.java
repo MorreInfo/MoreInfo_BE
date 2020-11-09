31eca9e3b7377eb7f47a36273054772766b3eedc
BENULL <327137362@qq.com>
2020-11-09 16:29:25 +0800

    add mybatis-generator common-lang3 guava

--- src/main/java/cn/edu/fudan/moreinfo/dao/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package cn.edu.fudan.moreinfo.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper {
-}
--- src/main/java/cn/edu/fudan/moreinfo/entity/User.java
+++
@@ -1,4 +0,0 @@
-package cn.edu.fudan.moreinfo.entity;
-
-public class User {
-}
