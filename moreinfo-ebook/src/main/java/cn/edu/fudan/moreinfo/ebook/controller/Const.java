53767271c74bd47524ae14029b1c393a7761c55a
BENULL <327137362@qq.com>
2020-11-18 07:40:56 +0800

    Revert "add member management"
    This reverts commit e59626b3

--- moreinfo-ebook/src/main/java/cn/edu/fudan/moreinfo/ebook/controller/Const.java
+++
@@ -1,7 +0,0 @@
-package cn.edu.fudan.moreinfo.ebook.controller;
-
-public class Const {
-  public static final String CURRENT_USER = "currentUser";
-  public static final String EMAIL = "email";
-  public static final String USERNAME = "username";
-}
